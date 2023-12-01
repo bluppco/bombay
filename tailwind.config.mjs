0e22b5ad9f595ea918cba7e37bee48e34d62ccce
Tanish Sharma <tanishsharmaab17@gmail.com>
2023-12-01 12:25:25 +0530

    feat: created masthead component on index page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
+			// Screen size breakpoints for responsive design
+			screens: {
+
+				"sm": "640px",   // Small screens, such as mobile phones
+				"md": "768px",   // Medium screens, such as tablets
+				"lg": "1024px",  // Large screens, such as laptops
+				"xl": "1280px",  // Extra-large screens
+				"mac": "1440px", // Screens with a width typical of macOS devices
+				"2xl": "1536px"  // Extra-extra-large screens
+
+			},
+
 			// Customizing Font Family
 			fontFamily: {
 
